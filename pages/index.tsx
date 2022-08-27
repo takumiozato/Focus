8fce6a5b77935c4df8e06eca60f2a771069d2a0b
takumiozato <dolphin.rokujiro@gmail.com>
2022-08-27 22:35:29 +0900

    setup Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Focus</title>
+        <meta name="description" content="It's Pomodoro Timer." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>Focus</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
